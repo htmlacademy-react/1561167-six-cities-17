f72f02b0045f7af143ecbb4632596d0e1064e5f4
Kholodov Vadym <kholodov.vadym@yandex.ru>
2024-12-29 19:27:11 +0300

    Добавит спинер

--- src/pages/loading-page/loadig-page.tsx
+++ src/pages/loading-page/loadig-page.tsx
@@ -1,5 +1,18 @@
+import { GridLoader } from 'react-spinners';
+import styles from './style.module.css';
+
 function LoadingPage(): JSX.Element {
-  return <p>Loading ...</p>;
+  return (
+    <div className={styles.wrapper}>
+      <GridLoader
+        color={'#4481C3'}
+        loading
+        size={25}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    </div>
+  );
 }
 
 export { LoadingPage };
