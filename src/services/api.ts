df605273ca0ab7274e3a39c75ed544aa9efa0f35
Kholodov Vadym <kholodov.vadym@yandex.ru>
2024-12-29 15:20:47 +0300

    Добавит токен в заголовок запросов

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,21 @@
 import axios, { AxiosInstance } from 'axios';
 import { Server } from '../const';
+import { getToken } from './token';
 
-const createAPI = (): AxiosInstance =>
-  axios.create({ baseURL: Server.Url, timeout: Server.Timeout });
+const createAPI = (): AxiosInstance => {
+  const api = axios.create({ baseURL: Server.Url, timeout: Server.Timeout });
+
+  api.interceptors.request.use((config) => {
+    const token = getToken();
+
+    if (token && config.headers) {
+      config.headers['x-token'] = token;
+    }
+
+    return config;
+  });
+
+  return api;
+};
 
 export { createAPI };
