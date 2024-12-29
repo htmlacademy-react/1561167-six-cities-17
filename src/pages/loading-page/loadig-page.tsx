b088cb7fc1f25f660b95c7b9a775a788558e1817
Kholodov Vadym <kholodov.vadym@yandex.ru>
2024-12-29 19:48:07 +0300

    Добавит центрирование для спинера

--- src/pages/loading-page/loadig-page.tsx
+++ src/pages/loading-page/loadig-page.tsx
@@ -3,14 +3,16 @@ import styles from './style.module.css';
 
 function LoadingPage(): JSX.Element {
   return (
-    <div className={styles.wrapper}>
-      <GridLoader
-        color={'#4481C3'}
-        loading
-        size={25}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      />
+    <div className="page page--gray page--main">
+      <div className={styles.wrapper}>
+        <GridLoader
+          color={'#4481C3'}
+          loading
+          size={25}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      </div>
     </div>
   );
 }
