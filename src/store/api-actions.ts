c2405b7d933ea95805046b54451af368c63f45ca
Kholodov Vadym <kholodov.vadym@yandex.ru>
2024-12-30 13:53:13 +0300

    Пофиксит замечания по типизации createAsyncThunk

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -3,51 +3,43 @@ import { AppDispatch, State } from '../types/state';
 import { AxiosInstance } from 'axios';
 import { APIRoute, AuthorizationStatus } from '../const';
 import { ShortOfferListType } from '../types/types';
-import { setAuthorizationStatus, setOffers, setOffersLoadingStatus } from './actions';
+import {
+  setAuthorizationStatus,
+  setOffers,
+  setOffersLoadingStatus,
+} from './actions';
 import { AuthorizationData, UserData } from '../types/user';
 import { dropToken, setToken } from '../services/token';
 
-const uploadOffersAction = createAsyncThunk<
-  void,
-  undefined,
-  {
-    dispatch: AppDispatch;
-    state: State;
-    extra: AxiosInstance;
-  }
->('offers/uploadOffers', async (_arg, { dispatch, extra: api }) => {
-  dispatch(setOffersLoadingStatus(true));
-  const { data } = await api.get<ShortOfferListType>(APIRoute.Offers);
-  dispatch(setOffersLoadingStatus(false));
-  dispatch(setOffers(data));
-});
+const createAppAsyncThunk = createAsyncThunk.withTypes<{
+  state: State;
+  dispatch: AppDispatch;
+  extra: AxiosInstance;
+}>();
 
-const checkAuthorizationAction = createAsyncThunk<
-  void,
-  undefined,
-  {
-    dispatch: AppDispatch;
-    state: State;
-    extra: AxiosInstance;
+const uploadOffersAction = createAppAsyncThunk<void, undefined>(
+  'offers/uploadOffers',
+  async (_arg, { dispatch, extra: api }) => {
+    dispatch(setOffersLoadingStatus(true));
+    const { data } = await api.get<ShortOfferListType>(APIRoute.Offers);
+    dispatch(setOffersLoadingStatus(false));
+    dispatch(setOffers(data));
   }
->('user/checkAuthorizationStatus', async (_arg, { dispatch, extra: api }) => {
-  try {
-    await api.get(APIRoute.Login);
-    dispatch(setAuthorizationStatus(AuthorizationStatus.Auth));
-  } catch {
-    dispatch(setAuthorizationStatus(AuthorizationStatus.NoAuth));
-  }
-});
+);
 
-const loginAction = createAsyncThunk<
-  void,
-  AuthorizationData,
-  {
-    dispatch: AppDispatch;
-    state: State;
-    extra: AxiosInstance;
+const checkAuthorizationAction = createAppAsyncThunk<void, undefined>(
+  'user/checkAuthorizationStatus',
+  async (_arg, { dispatch, extra: api }) => {
+    try {
+      await api.get(APIRoute.Login);
+      dispatch(setAuthorizationStatus(AuthorizationStatus.Auth));
+    } catch {
+      dispatch(setAuthorizationStatus(AuthorizationStatus.NoAuth));
+    }
   }
->(
+);
+
+const loginAction = createAppAsyncThunk<void, AuthorizationData>(
   'user/login',
   async ({ login: email, password }, { dispatch, extra: api }) => {
     const {
@@ -58,18 +50,13 @@ const loginAction = createAsyncThunk<
   }
 );
 
-export const logoutAction = createAsyncThunk<
-  void,
-  undefined,
-  {
-    dispatch: AppDispatch;
-    state: State;
-    extra: AxiosInstance;
+export const logoutAction = createAppAsyncThunk<void, undefined>(
+  'user/logout',
+  async (_arg, { dispatch, extra: api }) => {
+    await api.delete(APIRoute.Logout);
+    dropToken();
+    dispatch(setAuthorizationStatus(AuthorizationStatus.NoAuth));
   }
->('user/logout', async (_arg, { dispatch, extra: api }) => {
-  await api.delete(APIRoute.Logout);
-  dropToken();
-  dispatch(setAuthorizationStatus(AuthorizationStatus.NoAuth));
-});
+);
 
 export { uploadOffersAction, checkAuthorizationAction, loginAction };
