5926d938e4122f46f0dd04a419c4f827d62b6d24
Kholodov Vadym <kholodov.vadym@yandex.ru>
2025-01-28 00:16:09 +0300

    fix - Клик по кнопке "Sign out" приводит к выходу из закрытой части приложения

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './components/app/app';
 import { store } from './store';
-// import { checkAuthorizationStatus, uploadOffers } from './store/api-actions';
 import { ToastContainer } from 'react-toastify';
 import { ERROR_SHOW_TIMEOUT } from './const';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
-// store.dispatch(uploadOffers());
-// store.dispatch(checkAuthorizationStatus());
 
 root.render(
   <React.StrictMode>
