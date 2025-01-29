efb9b64cd8213cc9927116ed6d0d25f64975f772
Kholodov Vadym <kholodov.vadym@yandex.ru>
2025-01-29 20:15:29 +0300

    fix - В файле src/store/favorites/favorites-selectors.ts есть неиспользуемый селектор

--- src/store/favorites/favorites-selectors.ts
+++ src/store/favorites/favorites-selectors.ts
@@ -2,11 +2,6 @@ import { NameSpace } from '../../const';
 import { FavoritesListType } from '../../types/favorites';
 import { State } from '../../types/state';
 
-const selectOfferIsFavorite = (state: State, offerId: string): boolean =>
-  state[NameSpace.Favorites].favorites.findIndex(
-    (offer) => offer.id === offerId
-  ) !== -1;
-
 const selectFavorites = (state: State): FavoritesListType =>
   state[NameSpace.Favorites].favorites;
 
@@ -20,7 +15,6 @@ const selectFavoritesLoading = (state: State): boolean =>
   state[NameSpace.Favorites].isFavoritesLoading;
 
 export {
-  selectOfferIsFavorite,
   selectFavorites,
   selectFavoritesCount,
   selectChangingStaus,
