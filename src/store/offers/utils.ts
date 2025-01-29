0bfb969e36e0bd829db3fd80b998fb9612691c79
Kholodov Vadym <kholodov.vadym@yandex.ru>
2025-01-29 20:14:32 +0300

    fix - В файле src/store/offers/utils.ts есть неиспользуемый селектор

--- src/store/offers/utils.ts
+++
@@ -1,8 +0,0 @@
-import { ShortOfferListType } from '../../types/offers';
-
-const getOfferIndexById = (
-  offers: ShortOfferListType,
-  offerId: string
-): number => offers.findIndex((offer) => offer.id === offerId);
-
-export { getOfferIndexById };
